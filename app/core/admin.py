24517b05697c737b660a5434b348b9600bf6aeca
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2025-02-17 01:34:21 +0500

    Add tests for User model, Add custom User model, Customize admin panel for users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from core import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = [
+        [None, {'fields': ('email', 'password')}],
+        (
+            _('Permission'), {
+                'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        [
+            _('Important dates'), {'fields': ('last_login',)}
+        ]
+    ]
+    readonly_fields = ['last_login']
+    add_fieldsets = [
+        [None, {
+            'classes': ('wide',),
+            'fields': ('email',
+                       'password1',
+                       'password2',
+                       'name',
+                       'is_active',
+                       'is_staff',
+                       'is_superuser'),
+        }]
+    ]
+
+
+admin.site.unregister(Group)
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -20,8 +20,8 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    # @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_check):
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for a database when getting OperationalError"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
